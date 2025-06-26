4f8c343163135d25db1b11048446419f6f2a7861
Devileyes-blip <amankhandmc@gmail.com>
2025-06-26 13:24:05 +0530

    Add calculator with memory and expression eval
    Added an interactive calculator that lets users input expressions, stores the values, and performs calculations. It's designed for easy future improvements—open to suggestions on how to enhance it further!

---
+++ Interactive_calculator.py
@@ -0,0 +1,27 @@
+import sympy
+from sympy import *
+import streamlit as st
+
+st.title("🧮 Interactive Calculator")
+def cal():
+    '''Will calculate '''
+    if "history" not in st.session_state:
+        st.session_state.history = {}
+
+    inp = st.text_input("Enter the expression to calculate: ")
+    if inp:
+        try:
+            exp = sympify(inp).evalf()
+            if not exp.is_number:
+                st.error("Enter correct value which is Integers or numbers...")
+            else:
+                st.success(f"The answer is : {exp}")
+                st.session_state.history[inp] = exp
+        except Exception as e:
+            st.error(f"Error: {e}")
+        if st.toggle("Show History"):
+            st.subheader("📜Calcultion History")
+            for i, j in st.session_state.history.items():
+                st.write(f"{i} = {j}")
+               
+cal()
